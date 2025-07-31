8ed2187c49371af9989691d687eb19a69da2c71e
SammL1 <36447856+Samml1@users.noreply.github.com>
2025-07-31 13:42:13 +0100

    Samml1 day4branch (#3)
    * Update main.py
    
    adding new functionality into main for viewing stats and equiping weapon
    TO DO:
    - functionality for picking up items
    - functionality for dropping items
    - battle system
    
    * Update initialisePlayer.py
    
    updating initialisePlayer to have greater functionality - clsoer to update player object than initialise although still used to initialise player object
    
    * Update classStructure.py
    
    Extended player class with additional options
    
    Added new classes:
    Item: 
    - for items that can be found by player
    - slots into player inventory system
    
    weapon:
    - extends item
    - gives an item with flag isWeapon true damage values

--- initialisePlayer.py
+++ initialisePlayer.py
@@ -1,4 +1,4 @@
-from classStructure import player
+from classStructure import player, weapon, item
 
 # create a new player object when called from the main loop
 def newPlayer():
@@ -7,5 +7,28 @@ def newPlayer():
     user1 = player(userName, 'entrance')
 
     print("Welcome to your Adventure, " + user1.name + "!")
+
+    return user1
+
+def giveStartingItems():
+    sword = weapon("Sword","A small basic sword with no flare")
+    sword.isWeapon = True
+    sword.minDamage = 1
+    sword.maxDamage = 3
+
+    rope = item("Rope","Some rope that may come in handy later")
+    linen = item("Linen","A length of linen cloth")
+
+    startingItems = [sword, rope, linen]
+    return startingItems
+
+def equipWeapon(selectedWeapon, user1):
+    if selectedWeapon.isWeapon:
+        user1.playerHeldWeapon = selectedWeapon
+        user1.holdingWeapon = True
+        user1.inventory.remove(selectedWeapon)
+        print("you have equiped: " + user1.playerHeldWeapon.name)
+    else:
+        print("That is not a weapon")
 
     return user1
