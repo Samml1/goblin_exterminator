1a8e95bf3c1da1d3f3ca2ab1c3523995ee7f0e02
SammL1 <36447856+Samml1@users.noreply.github.com>
2025-07-31 13:39:25 +0100

    Update classStructure.py
    Extended player class with additional options
    
    Added new classes:
    Item: 
    - for items that can be found by player
    - slots into player inventory system
    
    weapon:
    - extends item
    - gives an item with flag isWeapon true damage values

--- classStructure.py
+++ classStructure.py
@@ -2,11 +2,27 @@ class player(object):
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.playerHP = int('10')
+        self.holdingWeapon = False
+        self.playerHeldWeapon = ""
+        self.inventory = []
 
 class room(object):
-    def __init__(self, roomName, description):
+    def __init__(self, name, roomName, description):
+        self.name = name
         self.roomName = roomName
-        self. description = description
+        self.description = description
+        self.enemyRoom = bool()
         self.exits = {}
-        self.enemies = {}
-        self.enemyRoom = False
+
+class item(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.isWeapon = False
+
+class weapon(item):
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.minDamage = int()
+        self.maxDamage = int()
