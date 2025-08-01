812fbdb20c8a7b93620d411f61ec5cc887584e66
SammL1 <36447856+Samml1@users.noreply.github.com>
2025-08-01 16:41:37 +0100

    Adding files via upload as SSH not working on laptop
    Updating all files to latest version
    - latest player movement
    - dungeon create from YML functionality

---
+++ createDungeonYML.py
@@ -0,0 +1,66 @@
+import yaml
+import io
+from yaml.loader import SafeLoader
+from classStructure import room
+from functools import reduce
+roomList = []
+
+# Take dungeon name and open YML file
+def yamlReader(fileName):
+    # open yaml file for loading
+    with open(fileName + '.yml', 'r') as f:
+        data = list(yaml.load_all(f, Loader=SafeLoader))
+
+    return data
+
+# Have room exits point to room objects for player movement
+def createExits(roomlist):
+    for i in range(0, len(roomList)):
+        print(roomList[i].name)
+        for j in range(0,len(roomList)):
+            if roomList[j].name in roomList[i].exits.values():
+                for x in roomList[i].exits:
+                    if roomList[i].exits[x] == roomList[j].name:
+                        roomList[i].exits[x] = roomList[j]
+
+    for y in range(0, len(roomList)):
+        print(roomList[y].name, roomList[y].exits)
+
+
+# Create the rooms for the dungeon
+def createRoom(roomId, roomName, description, enemyRoom, exits):
+    # from provided yaml data create object room
+    roomId = room(roomId, roomName, description)
+    roomId.enemyRoom = enemyRoom
+    roomId.exits = exits
+
+    # create list of rooms to be called on later
+    roomList.append(roomId)
+
+    return roomList
+
+# Print the values as a dictionary
+def defineDungeon(data):
+    #Loop list of yaml data entries
+    for i in range(0,len(data)):
+
+        room = data[i].get('room')
+        roomName = data[i].get('roomName')
+        description = data[i].get('description')
+        enemyRoom = bool(data[i].get('enemyRoom'))
+        exits = data[i].get('exits', {})
+        exits = reduce(lambda union, next_dict: union.update(next_dict) or union, exits, {})
+
+        #pass each yaml data entry to function
+        createRoom(room, roomName, description, enemyRoom, exits)
+
+    return room, roomName, description, enemyRoom, exits
+
+# Call this function from main.py to run through creation process
+def munchYaml(fileName):
+    data = yamlReader(fileName)
+    defineDungeon(data)
+    createExits(data)
+
+    print(roomList)
+    return roomList
---
+++ deprecated_files/createDungeon.py
@@ -0,0 +1,30 @@
+from classStructure import room
+
+# create a new dungeon instance as a list of room objects
+def createDungeon(dungeonName):
+
+    if dungeonName == "goblin cave":
+        # Create the dungeon rooms
+        entrance = room('entrance','Dungeon Entrance', 'The Entrance to the dungeon')
+        hallway = room('hallway','Dungeon hallway', 'A dark wet hallway that smells of mildew')
+        treasureRoom = room('treasureRoom','Treasure Room', 'A room full of a horde of treasure')
+
+        # Create exits
+        entrance.exits["north"] = hallway
+        hallway.exits["south"] = entrance
+        hallway.exits["east"] = treasureRoom
+        treasureRoom.exits["west"] = hallway
+
+        # Define enemy rooms
+        entrance.enemyRoom = False
+        hallway.enemyRoom = False
+        treasureRoom.enemyRoom = True
+
+        roomList = [entrance, hallway, treasureRoom]
+    elif dungeonName == "q":
+        exit()
+    else:
+        dungeonName = input("No dungeon by that name found\nplease enter name again\n")
+        createDungeon(dungeonName)
+
+    return roomList
